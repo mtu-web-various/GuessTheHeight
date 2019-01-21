aca4854fbb431a2cbedbb7094cb5cff92f1cf6de
Talha <talhamucar@gmail.com>
2019-01-20 22:34:55 -0500

    final

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //after DOM loaded
 document.addEventListener('DOMContentLoaded', function() {
   var playerCount = 1;
-  var guessCount = 4;
+  var guessCount = 3;
   var theHeight;
   var otherInfo;
+  var playerTag
   choosePlayer();
 
   //Start Button Toggling between screens and call startGame
@@ -24,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
   //Showing the height list
   function startGame() {
     // show the player info 
-    var playerTag = document.getElementById("player-info");
+    playerTag = document.getElementById("player-info");
     playerTag.innerText = otherInfo;
 
     // get random words and append them to the DOM
@@ -36,6 +37,42 @@ document.addEventListener('DOMContentLoaded', function() {
       li.innerText = h;
       heightList.appendChild(li);
     });
+
+    setGuessCount(guessCount);
+
+    // add update listener for clicking on a word
+    heightList.addEventListener('click', updateGame);
+  }
+
+  function updateGame(e) {
+    if (e.target.tagName === "LI" && !e.target.classList.contains("disabled")){
+      // grab guessed word, check it against password, update view
+      var guess = e.target.innerText;
+      var result = comparePlayers(guess);
+      e.target.classList.add("disabled");
+      if (result === 0){ e.target.innerText = e.target.innerText + " --> You get it right!"; }
+      else if (result === 1){ e.target.innerText = e.target.innerText + " --> Lower!"; }
+      else {e.target.innerText = e.target.innerText + " --> Higher!";}
+      setGuessCount(guessCount - 1);  
+      console.log(theHeight);
+
+      // check whether the game is over
+      if (result === 0) {
+        toggleClasses(document.getElementById("winner"), 'hide', 'show');
+        this.removeEventListener('click', updateGame);
+      } else if (guessCount === 0) {
+        toggleClasses(document.getElementById("loser"), 'hide', 'show');
+        this.removeEventListener('click', updateGame);
+        playerTag.innerText = otherInfo + theHeight + ":(";
+      }
+    }
+  }
+
+  //if true return 0, less -1, high 1
+  function comparePlayers(guess){
+    if (guess > theHeight){ return 1; }
+    else if (guess < theHeight){ return -1; }
+    else {return 0;}
   }
 
   function choosePlayer(){
@@ -47,5 +84,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 players[chosen]["Team Name"] + " / "; 
   }
 
+  function setGuessCount(newCount) {
+    guessCount = newCount;
+    document.getElementById("guesses-remaining").innerText = "Guesses remaining: " + guessCount + ".";
+  }
 
 });
